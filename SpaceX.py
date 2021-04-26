75ba5898dd37b9c50cb2d881e4b81ceec90e1ea8
pawelsiela <83124904+pawelsiela@users.noreply.github.com>
2021-04-26 21:46:28 +0200

    Add files via upload

---
+++ SpaceX.py
@@ -0,0 +1,31 @@
+import requests
+import csv
+
+class Flight:
+    def __init__(self, flight_number, mission_name, rocket_id, rocket_name, launch_date_utc, video_link):
+        self.rocket_id = rocket_id
+        self.rocket_name = rocket_name
+        self.launch_date_utc = launch_date_utc
+        self.video_link = video_link
+        self.mission_name = mission_name
+        self.flight_number = flight_number
+
+flights = []
+try:
+    r = requests.get('https://api.spacexdata.com/v3/launches').json()
+except:
+    raise Exception('There is a problem to connect with SpaceX api.')
+
+for flight in r:
+    flights.append(Flight(flight['rocket']['rocket_id'],
+     flight['mission_name'], 
+     flight['rocket']['rocket_id'], 
+     flight['rocket']['rocket_name'], 
+     flight['launch_date_utc'], 
+     flight['links']['video_link']))
+
+with open('flights.csv', 'a') as f:
+    writer = csv.writer(f)
+
+    for flight in flights:
+        writer.writerow([flight.flight_number, flight.mission_name, flight.rocket_id, flight.rocket_name, flight.launch_date_utc, flight.video_link])
